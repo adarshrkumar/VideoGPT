a51343616bdcc7edf024e71e30970b1ec1d384a1
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-12-26 19:41:27 -0800

    update index.mjs

--- index.mjs
+++ index.mjs
@@ -5,32 +5,44 @@ import fs from 'fs';
 import express from 'express';
 const app = express();
 
+const url = "https://api.edenai.run/v2/workflow/9c7ef864-8d59-4ebf-87c6-3fde471dc10b/execution/"
 
-app.get('/', (req, res) => {
+async function getExecution(id, res) {
+  const response = await fetch(`${url}/${id}`.replaceAll('//', '/'), {
+    headers: {
+      "Content-Type": "application/json",
+      'Authorization': `Bearer ${process.env.TOKEN}`
+    },
+  })
+  const result = await response.json();
+
+  if (result.content.status === 'processing') {
+    setTimeout(() => getExecution(id, res), 1000);
+    return
+  }
+
+  res.send(result.content.results.image__background_removal);
+}
+
+app.get('/', async (req, res) => {
   const form = new FormData();
 
-  form.append('format', 'PNG');
-  form.append('output_type', 'cutout');
-  form.append('image_url', './test-image.jpg');
+  form.append('file', fs.createReadStream('./test-image.jpg'), 'test-image.jpg');
 
-  fetch("https://api.picsart.io/tools/1.0/removebg", {
-    method: "POST",
+  const response = await fetch(url, {
+    method: 'POST',
     headers: {
-      'x-picsart-api-key': process.env.PICSART_API_KEY || 'eyJraWQiOiI5NzIxYmUzNi1iMjcwLTQ5ZDUtOTc1Ni05ZDU5N2M4NmIwNTEiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhdXRoLXNlcnZpY2UtMTY0OTZmODYtOTJhZi00OGIyLTk4YWYtZTgwODg3MjUxNWE4IiwiYXVkIjoiNDY5NTk4NzIyMDE5MTAxIiwibmJmIjoxNzM1MjY3ODY5LCJzY29wZSI6WyJiMmItYXBpLmdlbl9haSIsImIyYi1hcGkuaW1hZ2VfYXBpIl0sImlzcyI6Imh0dHBzOi8vYXBpLnBpY3NhcnQuY29tL3Rva2VuLXNlcnZpY2UiLCJvd25lcklkIjoiNDY5NTk4NzIyMDE5MTAxIiwiaWF0IjoxNzM1MjY3ODY5LCJqdGkiOiI4NDVhZDMyNS1kMDE0LTQwMzItODIyZS04MzI5ZWQzNDRmYmQifQ.U9tz_pcIfDTG7gC5X9JXPXZnOZBhB2rPbeCHALGskcIobLJ-Jf4L7VfgI92w_ajiAPtEuyMIuESZbrjnRTg1vavPVCAOlr6UWEbzbyZYbaMhjqtJ4BSoZWc1Dk6WSB1YOq06PdpxzsympSwWsNT3BsDMFFvRa5EaXl8DW6LD6qEadIFteFTDwQ5n8qCBmJbzYWCB1-Xo8S6BVtoWQylJQCWcx44u2xk8x0M0fbqgSA5dpTroluCSoMLvFkRdCgNbhB05eJW1dkQw8ERiwN6E5owA6IsHCyqxdQnRQQS84cBMUeCKZTgEPvVf_E5UkQpWp7fKZiUlUGTAjZ14pSmrTA',
+      ...form.getHeaders(), // Add FormData headers
+      'Authorization': `Bearer ${process.env.TOKEN}`
     },
     body: form
   })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error("Something went wrong.");
-    })
-    .then((response) => {
-      res.send(response.data);
-    })
-    .catch((error) => console.error(error));
-  
+    .then(response => response.json())
+    .then(json  => getExecution(json.id, res))
+});
+
+app.get('/getExecution', async (req, res) => {
+  getExecution(req.query.id, res)
 });
 
 app.listen(3000, () => {
