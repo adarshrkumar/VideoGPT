15872264c34fe6ceaa7f71a1304e58c7fe0c52d4
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-12-30 13:10:21 -0800

    update files

--- index.mjs
+++ index.mjs
@@ -7,31 +7,55 @@ import express from 'express';
 const app = express();
 
 import useErrorTemplate from './error.mjs';
-import { url } from 'inspector';
 
 async function getExecution(id, res, i) {
   const task = await client.tasks.retrieve(id);
-  console.log(task);
-
-  if (i > 60) {
-    res.status(408).send(useErrorTemplate(408, `Session Timeout, please try again later.\nYou can get the execution again on its own by using the /getExecution endpoint with the id of the execution which is "${id}"`))
-    return
-  }
-
-  // switch (result.content.status) {
-  //   case 'succeded':
-  //     res.send(result.content.result.results.image__background_removal);
-  //     break;
-  //   case 'processing': 
-  //     setTimeout(() => getExecution(id, res, i++), 5000);
-  //     break
-  //   default:
-      res.json(task);
-  // }
+  res.json(task);
 }
 
-app.get('/', async (req, res) => {
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
 
+    // Uploads is the Upload_folder_name
+    callback(null, 'temp')
+  },
+  filename: function (req, file, callback) {
+    fName = file.originalname
+    callback(null, file.originalname)
+  }
+})
+
+// Define the maximum size for uploading
+// picture i.e. 1 MB. it is optional
+const maxSize = 1000 * 1000 * 1000;
+
+var upload = multer({
+  storage: storage,
+  limits: { fileSize: maxSize },
+  fileFilter: function (req, file, callback) {
+
+    // Set the filetypes, it is optional
+    var filetypes = /jpeg|jpg|png|webp|gif/;
+    var mimetype = filetypes.test(file.mimetype);
+
+    var extname = path.extname(file.originalname).toLowerCase()
+    extname = filetypes.test(extname);
+
+    if (mimetype && extname) {
+      fName = file.originalname
+      return callback(null, fName);
+    }
+
+    callback('Error: File upload only supports the following filetypes - ' + filetypes, null);
+  }
+  // mypic is the name of file attribute
+}).single('image');
+
+app.get('/', (req, res) => {
+  res.redirect('/generate');
+})
+
+app.get('/generate', async (req, res) => {
   // The env var RUNWAYML_API_SECRET is expected to contain your API key.
   const client = new RunwayML();
   
@@ -41,25 +65,49 @@ app.get('/', async (req, res) => {
       promptText: req.query.prompt,
     });
     res.json({id: task.id});
-
-    // res.json(getExecution(task.id, res, 0))
 });
 
 app.get('/getExecution', (req, res) => {
-  getExecution(req.query.id, res, 0)
+  getExecution(req.query.id, res)
 });
 
-app.get('/base64Upload', (req, res) => {
-  var fPath = decodeURIComponent(req.query.path) || 'error';
-  var path = fPath.replaceAll('/', '_')
-  var content = req.query.b64content
-
-  if (!fs.existsSync('./temp')) fs.mkdirSync('./temp');
-
-  if (fName !== 'error') fs.writeFileSync(`./temp/${path}.png`, content, 'base64')
-
-  res.json({status: 'success', message: `Wrote to file "${fPath}" please use that as the url param please`, url: `${req.protocol}//${req.host}/temp/${path}.png`})
+app.get('/upload', (req, res) => {
+  res.sendFile('/upload.html', { root: '.' });
 });
+
+app.post('/uploadFile', (req, res) => {
+  // Error MiddleWare for multer file upload, so if any
+  // error occurs, the image would not be uploaded!
+  upload(req, res, function(err) {
+    if(err) {
+      // ERROR occurred (here it can be occurred due
+      // to uploading image of size greater than
+      // 1MB or uploading different file type)
+      res.send(err)
+    }
+    else {
+      // SUCCESS, image successfully uploaded
+      // res.send(fName)
+      var url = `/chat?`
+      
+      var hasParent = req.query.hasParent
+      if (!!hasParent) url = `/upload?sucess=true&`
+      
+      url += `filelocation=temp-storage&name=${fName}`
+      
+      var p = req.query.p
+      if (!!p) url += `&prompt=${p}`
+
+      var t = req.query.t
+      if (!!t) url += `&type=${t}`
+
+      var isBulk = req.query.isBulk
+      if (!!isBulk) url += `&isBulk=${isBulk}`
+      
+      res.redirect(url)
+    }
+  })
+})
 
 app.get('/temp/:file', (req, res) => {
   if (!fs.existsSync(`/temp/${req.params.file}`)) {
@@ -69,6 +117,18 @@ app.get('/temp/:file', (req, res) => {
 
   res.sendFile(`/temp/${req.params.file}` , { root : '.' });
 });
+
+app.get('*', (req, res) =>{
+  var path = req.path
+  if (path.startsWith('/')) path = path.slice(1)
+  if (path.endsWith('/')) path = path.slice(0, -1)
+  if (!path.includes('.')) path = `${path}.html`
+  if (!fs.existsSync(`./${path}`)) {
+    useErrorTemplate(404, `Page not found: ${path}`)
+    return
+  }
+  res.sendFile(path, {root: '.'})
+})
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
